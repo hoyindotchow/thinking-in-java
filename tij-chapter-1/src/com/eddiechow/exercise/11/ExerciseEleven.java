095fdb5562554dc516631c69963e54254fc2f4cd
Eddie <hoyindotchow@gmail.com>
2016-08-26 20:47:01 -0700

    completed exercise 11

---
+++ tij-chapter-1/src/com/eddiechow/exercise/11/ExerciseEleven.java
@@ -0,0 +1,15 @@
+package com.eddiechow.exercise.eleven;
+
+import com.eddiechow.exercise.eleven.AllTheColoursOfTheRainbow;
+
+public class ExerciseEleven {
+
+	public static void main(String[] args){
+		
+	AllTheColoursOfTheRainbow colourInt = new AllTheColoursOfTheRainbow();
+	colourInt.changeTheColourOfTheRainbow(713);
+	
+	System.out.println(colourInt.anIntegerRepresentingColours);
+
+	}
+}
