24d7e628e6624d6a7ff57aa1020e083c4cb432b4
Eddie <hoyindotchow@gmail.com>
2017-07-26 17:57:31 -0700

    added Exercise seven

---
+++ tij-chapter-2/src/com/eddiechow/exercise/seven/CoinFlipper.java
@@ -0,0 +1,18 @@
+package com.eddiechow.exercise.seven;
+
+public class CoinFlipper {
+
+	
+	
+	void coinFlip(int num){
+		
+		if (num % 2 == 0){
+			System.out.println("Heads");
+		}
+		else{
+			System.out.println("Tails");
+		}
+
+	}
+
+}
