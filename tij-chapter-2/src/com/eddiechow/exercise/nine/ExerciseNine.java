75babbe9d4ca9c603f33bde8ec464acb73d91f9e
Eddie <hoyindotchow@gmail.com>
2017-08-11 17:29:00 -0700

    adding exercise nine and ten. Skipping the rest of them cause they're dumb.

---
+++ tij-chapter-2/src/com/eddiechow/exercise/nine/ExerciseNine.java
@@ -0,0 +1,11 @@
+package com.eddiechow.exercise.nine;
+
+public class ExerciseNine {
+	public static void main(String args[]){
+		System.out.println("Long Max: " + Long.MAX_VALUE);
+		System.out.println("Long Min: " + Long.MIN_VALUE);
+		System.out.println("Double Max: " + Double.MAX_VALUE);
+		System.out.println("Double Min: " + Double.MIN_VALUE);
+	}
+
+}
